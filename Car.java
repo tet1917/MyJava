84827071d6b160c304602fcd7f20b55ab428c176
tet1917 <tetsu0224.soccer@icloud.com>
2023-02-16 20:00:36 +0900

    java youtube practice

--- Car.java
+++ Car.java
@@ -1,19 +1,18 @@
 class Car {
+  //ナンバー変数
   int no;
   int speed;
 
+  //メソッド
   void setNo(int n) {
     no = n;
   }
-
   void run(int s) {
     speed = s;
   }
-
   void stop() {
     speed = 0;
   }
-
   void display() {
     System.out.println("ナンバー"+ no + "の速度は" + speed + "です。");
   }
--- DriveCar.java
+++ DriveCar.java
@@ -1,9 +1,12 @@
 class DriveCar {
   public static void main(String[] args){
+    //オブジェクトを生成
     Car c1 = new Car();
 
     c1.setNo(2525);
     c1.run(30);
-
+    c1.display();
+    c1.stop();
+    c1.display();
   }
 }
